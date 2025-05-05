ec25ed5b87310c8838f9039b8e358f12237ba2d8
racheliKisner <racheli9127@gmail.com>
2025-05-05 20:34:01 +0300

    chek 05/05 20:33

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,13 +2,16 @@
 import { CartComponentPopUp } from '../components/Cart/CartComponentPopUp';
 import { useSelector } from 'react-redux';
 import ProductPage from '../components/ChosenProducts';
+import img from '../../src/assets/img2/imgCamping.jpg';
+import { ImgComponent } from '../components/ImgComponent';
 export const Home = ()=>{
     return (
         <> 
                 <h1>Hi welcome to the Home Page.</h1>
                 {/* <FontAwesomeIcon icon="fa-light fa-cart-shopping" /> */}
-                {/* <CartComponentPopUp></CartComponentPopUp> */}
+                <CartComponentPopUp></CartComponentPopUp>
                 {/* <RouterComponent></RouterComponent> */}
+                <ImgComponent path={img} className="product-img" />
               <ProductPage></ProductPage>
         </>
 
