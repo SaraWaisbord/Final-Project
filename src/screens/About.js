1a3b4beff955d4a4fea2e076eaab0f092b2fcca5
SaraWaisbord <s0504119750@gmail.com>
2025-05-06 17:45:18 +0300

    new branch2

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,11 +3,17 @@
 
 import QuantityControl from '../components/QantityButton.js'
 export const About = ()=>{
-    return(<><h1>welcome to About page
-
-    </h1>
-    <p>this is a test</p>
-    </>
+    return(
+        <div className='about'>
+            <h1>About Us</h1>
+            <p>We are a travel product company dedicated to providing the best products for your travel needs.</p>
+            <h2>Our Mission</h2>
+            <p>To make travel easier and more enjoyable for everyone.</p>
+            <h2>Our Team</h2>
+            <p>We have a team of experienced professionals who are passionate about travel and customer service.</p>
+            <h2>Contact Us</h2>
+            
+        </div>
     
     );
 }
